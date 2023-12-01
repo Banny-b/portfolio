a1448152a870105720aecc8c75c459d6f213e277
Sergey Petukhov <banny75@i.ua>
2023-12-01 18:29:43 +0200

    portfolio
    Update modal

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,50 @@
-const openPopUp = document.getElementById('open_pop_up'); // Второй код написать через класс (document.querySelector())
-const closePopUp = document.getElementById('pop_up_close');
-const popUp = document.getElementById('pop_up');
+// Вариант с алертом
+const openPopUp = document.querySelector('.about-info__link');
+const closePopUp = document.querySelector('.modal-close');
+const popUp = document.querySelector('.backdrop');
+const modalForm = document.querySelector('.modal-form');
+const userEmailInput = document.querySelector('.modal-form__input');
 
-openPopUp.addEventListener('click', function(e){
+openPopUp.addEventListener('click', function (e) {
     e.preventDefault();
     popUp.classList.add('active');
 });
-
-closePopUp.addEventListener('click', () => {
+closePopUp.addEventListener('click', function () {
     popUp.classList.remove('active');
-})
+});
+modalForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Проверяем валидацию формы
+    if (modalForm.checkValidity()) {
+        popUp.classList.remove('active');
+        alert('This is a test window!');
+    }
+});
+
+
+// Простой вариант через id
+// const openPopUp = document.getElementById('open_pop_up');
+// const closePopUp = document.getElementById('pop_up_close');
+// const popUp = document.getElementById('pop_up');
+
+// openPopUp.addEventListener('click', function(e){
+//     e.preventDefault();
+//     popUp.classList.add('active');
+// });
+// closePopUp.addEventListener('click', () => {
+//     popUp.classList.remove('active');
+// });
+
+
+// Простой вариант через классы (querySelector())
+// const openPopUp = document.querySelector('.about-info__link');
+// const closePopUp = document.querySelector('.modal-close');
+// const popUp = document.querySelector('.backdrop');
+
+// openPopUp.addEventListener('click', function(e){
+//     e.preventDefault();
+//     popUp.classList.add('active');
+// });
+// closePopUp.addEventListener('click', () => {
+//     popUp.classList.remove('active');
+// });
