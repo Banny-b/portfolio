dc22c44db0512264cd80ad200f001b07fe5e2a35
Sergey Petukhov <banny75@i.ua>
2024-03-12 17:48:40 +0200

    portfolio
    Add Form submission

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,49 @@
-// Вариант с алертом
+// С алертом, что не бот!
 const openPopUp = document.querySelector('.about-info__link');
 const closePopUp = document.querySelector('.modal-close');
 const popUp = document.querySelector('.backdrop');
-const modalForm = document.querySelector('.modal-form');
-const userEmailInput = document.querySelector('.modal-form__input');
 
-openPopUp.addEventListener('click', function (e) {
+openPopUp.addEventListener('click', function(e){
     e.preventDefault();
-    popUp.classList.add('active');
-});
-closePopUp.addEventListener('click', function () {
-    popUp.classList.remove('active');
-});
-modalForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    // Проверяем валидацию формы
-    if (modalForm.checkValidity()) {
-        popUp.classList.remove('active');
-        alert('This is a test window!');
+    // Добавляем условие проверки на подтверждение, что пользователь не бот
+    const isUserNotBot = confirm('To send a message you need to confirm that you are not a bot. Then wait for the form to submit (depending on Internet speed).');
+    if (isUserNotBot) {
+        popUp.classList.add('active');
+    } else {
+        // Если пользователь не подтвердил, не открываем модальное окно
+        alert('Action cancelled. Please confirm that you are not a bot!');
     }
 });
+
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+});
+
+
+
+// Вариант с алертом "Тест"
+// const openPopUp = document.querySelector('.about-info__link');
+// const closePopUp = document.querySelector('.modal-close');
+// const popUp = document.querySelector('.backdrop');
+// const modalForm = document.querySelector('.modal-form');
+// const userEmailInput = document.querySelector('.modal-form__input');
+
+// openPopUp.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     popUp.classList.add('active');
+// });
+// closePopUp.addEventListener('click', function () {
+//     popUp.classList.remove('active');
+// });
+// modalForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     // Проверяем валидацию формы
+//     if (modalForm.checkValidity()) {
+//         popUp.classList.remove('active');
+//         alert('This is a test window!');
+//     }
+// });
+
 
 
 // Простой вариант через id
@@ -34,6 +58,7 @@ modalForm.addEventListener('submit', function (e) {
 // closePopUp.addEventListener('click', () => {
 //     popUp.classList.remove('active');
 // });
+
 
 
 // Простой вариант через классы (querySelector())
